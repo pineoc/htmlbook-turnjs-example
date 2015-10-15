4b5c95ca77d285a8256d75dd2e2ea1e85afb3623
mac_mini <pineoc@naver.com>
2015-10-15 20:45:41 +0900

    video problem fixing...

--- platforms/ios/www/js/book_index.js
+++ platforms/ios/www/js/book_index.js
@@ -1,11 +1,36 @@
 /*
  * Turn.js responsive book
  */
+
+/*
+turn corner change
+
+var way = 1;
+
+setInterval(function() {
+  if (way==1) {
+    $("#flipbook").turn("next");
+
+    if ($("#flipbook").turn("page")== $("#flipbook").turn("pages")) {
+      way = 2;
+      $("#flipbook").turn("options", {turnCorners: "tl,tr"});
+    }
+  } else {
+    $("#flipbook").turn("previous");
+    
+    if ($("#flipbook").turn("page")==1) {
+       way = 1;
+       $("#flipbook").turn("options", {turnCorners: "bl,br"});
+    }
+  }
+}, 1000);
+
+*/
 (function () {
     'use strict';
 
     var module = {
-        ratio: 0.75, //1.38,
+        ratio: 0.75, //double: 1.38, single: 0.75
         currPage: 1,
         init: function (id) {
             var me = this;
@@ -22,6 +47,7 @@
                     $(me.el).turn('size', size.width, size.height);
                 });
             }
+
             $("#book").bind("turning", function (event, page, view) {
                 document.getElementById("book_effect").play();
                 this.currPage = page;
@@ -55,7 +81,7 @@
         plugins: function (pageNum) {
             // run the plugin
             $(this.el).turn({
-                page: 23,
+                page: 1,
                 display: "single",
                 gradients: true,
                 acceleration: true
@@ -68,14 +94,113 @@
     module.init('book');
 }());
 
-var onDeviceReady = function () {
-    $("#okgoLink").tap(function () {
-        var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+//swipe function
+$(function () {
+    'use strict';
+    //Enable swiping...
+    $(".t").swipe({
+        //Generic swipe handler for all directions
+        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+            if (direction === 'left') {
+                $("#book").turn("next");
+            } else {
+                $("#book").turn("previous");
+            }
+        },
+        //Default is 75px, set to 0 for demo so any distance triggers swipe
+        threshold: 75
     });
+});
+
+//okay go link function
+$(function () {
+    'use strict';
+    var linkfunc = function () {
+        $("#okgoLink").tap(function () {
+            var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+        });
+    };
+
+    var init = function () {
+        document.addEventListener('deviceready', linkfunc, false);
+    };
+
+    $(document).ready(init);
+});
+/*
+$(function () {
+    'use strict';
+    var myFilename = "turning.mp4";
+    var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
+    var fileTransfer = new FileTransfer();
+    var filePath = cordova.file.dataDirectory + myFilename;
+
+    fileTransfer.download(encodeURI(myUrl), filePath, (function (entry) {
+        var vid = document.getElementById("videoTurning");
+        vid.src = entry.nativeURL;
+        vid.loop = false;
+    }), (function (error) {
+        alert("Video download error: source " + error.source);
+        alert("Video download error: target " + error.target);
+    }), true, {
+        headers: {
+            Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+        }
+    });
+});
+*/
+
+//video file setup function
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function () {
+        if ("Android" === device.platform) {
+            //video src set
+            var myFilename = "turning.mp4";
+            var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
+            var fileTransfer = new FileTransfer();
+            var filePath = cordova.file.dataDirectory + myFilename;
+            alert('myUrl : ' + myUrl);
+            alert('filePath : ' + filePath);
+
+            fileTransfer.download(encodeURI(myUrl), filePath,
+                function (entry) {
+                    var vid = document.getElementById("videoTurning");
+                    vid.src = entry.nativeURL;
+                    vid.loop = false;
+                    alert('download success');
+                    alert('entry,nativeURL : ' + entry.nativeURL);
+                    alert('vid src : ' + vid.src);
+                },
+                function (error) {
+                    alert("Video download error: source " + error.source);
+                    alert("Video download error: target " + error.target);
+                }, true, {
+                    headers: {
+                        Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                    }
+                });
+            alert('download ended');
+        }
+    }
 };
 
-var init = function () {
-    document.addEventListener('deviceready', onDeviceReady, false);
-};
-
-$(document).ready(init);
+app.initialize();
--- www/js/book_index.js
+++ www/js/book_index.js
@@ -1,6 +1,31 @@
 /*
  * Turn.js responsive book
  */
+
+/*
+turn corner change
+
+var way = 1;
+
+setInterval(function() {
+  if (way==1) {
+    $("#flipbook").turn("next");
+
+    if ($("#flipbook").turn("page")== $("#flipbook").turn("pages")) {
+      way = 2;
+      $("#flipbook").turn("options", {turnCorners: "tl,tr"});
+    }
+  } else {
+    $("#flipbook").turn("previous");
+    
+    if ($("#flipbook").turn("page")==1) {
+       way = 1;
+       $("#flipbook").turn("options", {turnCorners: "bl,br"});
+    }
+  }
+}, 1000);
+
+*/
 (function () {
     'use strict';
 
@@ -71,14 +96,14 @@
 
 //swipe function
 $(function () {
+    'use strict';
     //Enable swiping...
     $(".t").swipe({
         //Generic swipe handler for all directions
         swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-            if(direction==='left') {
+            if (direction === 'left') {
                 $("#book").turn("next");
-            }
-            else {
+            } else {
                 $("#book").turn("previous");
             }
         },
@@ -87,15 +112,95 @@ $(function () {
     });
 });
 
+//okay go link function
+$(function () {
+    'use strict';
+    var linkfunc = function () {
+        $("#okgoLink").tap(function () {
+            var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+        });
+    };
 
-var onDeviceReady = function () {
-    $("#okgoLink").tap(function () {
-        var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+    var init = function () {
+        document.addEventListener('deviceready', linkfunc, false);
+    };
+
+    $(document).ready(init);
+});
+/*
+$(function () {
+    'use strict';
+    var myFilename = "turning.mp4";
+    var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
+    var fileTransfer = new FileTransfer();
+    var filePath = cordova.file.dataDirectory + myFilename;
+
+    fileTransfer.download(encodeURI(myUrl), filePath, (function (entry) {
+        var vid = document.getElementById("videoTurning");
+        vid.src = entry.nativeURL;
+        vid.loop = false;
+    }), (function (error) {
+        alert("Video download error: source " + error.source);
+        alert("Video download error: target " + error.target);
+    }), true, {
+        headers: {
+            Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+        }
     });
+});
+*/
+
+//video file setup function
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+        app.receivedEvent();
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function () {
+        if ("Android" === device.platform) {
+            //video src set
+            var myFilename = "turning.mp4";
+            var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
+            var fileTransfer = new FileTransfer();
+            var filePath = cordova.file.dataDirectory + myFilename;
+            alert('myUrl : ' + myUrl);
+            alert('filePath : ' + filePath);
+
+            fileTransfer.download(encodeURI(myUrl), filePath,
+                function (entry) {
+                    var vid = document.getElementById("videoTurning");
+                    vid.src = entry.nativeURL;
+                    vid.loop = false;
+                    alert('download success');
+                    alert('entry,nativeURL : ' + entry.nativeURL);
+                    alert('vid src : ' + vid.src);
+                },
+                function (error) {
+                    alert("Video download error: source " + error.source);
+                    alert("Video download error: target " + error.target);
+                }, true, {
+                    headers: {
+                        Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+                    }
+                });
+            alert('download ended');
+        }
+    }
 };
 
-var init = function () {
-    document.addEventListener('deviceready', onDeviceReady, false);
-};
-
-$(document).ready(init);
+app.initialize();
