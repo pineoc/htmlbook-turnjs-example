7affb0363c03b6b8cb2be6d3ce6bf57106364918
mac_mini <pineoc@naver.com>
2015-10-16 17:00:30 +0900

    loading, flip option... changed
    Orientation protrait => all
    video can see it
    sound not fixed
    almost done

--- www/js/book_index.js
+++ www/js/book_index.js
@@ -26,129 +26,84 @@ setInterval(function() {
 }, 1000);
 
 */
-(function () {
-    'use strict';
 
-    var module = {
-        ratio: 0.75, //double: 1.38, single: 0.75
-        currPage: 1,
-        init: function (id) {
-            var me = this;
+//book, turn.js setup
+var module = {
+    ratio: 0.75, //double: 1.38, single: 0.75
+    currPage: 1,
+    init: function (id) {
+        'use strict';
+        var me = this;
 
-            // if older browser then don't run javascript
-            if (document.addEventListener) {
-                this.el = document.getElementById(id);
-                this.resize();
-                this.plugins();
+        // if older browser then don't run javascript
+        if (document.addEventListener) {
+            this.el = document.getElementById(id);
+            this.resize();
+            this.plugins();
 
-                // on window resize, update the plugin size
-                window.addEventListener('resize', function (e) {
-                    var size = me.resize();
-                    $(me.el).turn('size', size.width, size.height);
-                });
-            }
-
-            $("#book").bind("turning", function (event, page, view) {
-                document.getElementById("book_effect").play();
-                this.currPage = page;
+            // on window resize, update the plugin size
+            window.addEventListener('resize', function (e) {
+                var size = me.resize();
+                $(me.el).turn('size', size.width, size.height);
             });
-
-        },
-        resize: function () {
-            // reset the width and height to the css defaults
-            this.el.style.width = '';
-            this.el.style.height = '';
-
-            var width = this.el.clientWidth,
-                height = Math.round(width / this.ratio),
-                padded = Math.round(document.body.clientHeight * 0.9);
-
-            // if the height is too big for the window, constrain it
-            if (height > padded) {
-                height = padded;
-                width = Math.round(height * this.ratio);
-            }
-
-            // set the width and height matching the aspect ratio
-            this.el.style.width = width + 'px';
-            this.el.style.height = height + 'px';
-
-            return {
-                width: width,
-                height: height
-            };
-        },
-        plugins: function (pageNum) {
-            // run the plugin
-            $(this.el).turn({
-                page: 1,
-                display: "single",
-                gradients: true,
-                acceleration: true
-            });
-            // hide the body overflow
-            document.body.className = 'hide-overflow';
         }
-    };
 
-    module.init('book');
-}());
-
-//swipe function
-$(function () {
-    'use strict';
-    //Enable swiping...
-    $(".t").swipe({
-        //Generic swipe handler for all directions
-        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-            if (direction === 'left') {
-                $("#book").turn("next");
-            } else {
-                $("#book").turn("previous");
-            }
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold: 75
-    });
-});
-
-//okay go link function
-$(function () {
-    'use strict';
-    var linkfunc = function () {
-        $("#okgoLink").tap(function () {
-            var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+        $("#book").bind("turning", function (event, page, view) {
+            document.getElementById("book_effect").play();
+            this.currPage = page;
         });
-    };
+        $("#book").bind("start", function (event, page, corner) {
+            console.log('page : ', page);
+            if ('bl' == corner) {
+                event.preventDefault();
+                page.next = page.page;
+            }
+            if ('br' == corner) {
+                event.preventDefault();
+                page.next = page.page;
+            }
+        });
 
-    var init = function () {
-        document.addEventListener('deviceready', linkfunc, false);
-    };
+    },
+    resize: function () {
+        // reset the width and height to the css defaults
+        'use strict';
+        this.el.style.width = '';
+        this.el.style.height = '';
 
-    $(document).ready(init);
-});
-/*
-$(function () {
-    'use strict';
-    var myFilename = "turning.mp4";
-    var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
-    var fileTransfer = new FileTransfer();
-    var filePath = cordova.file.dataDirectory + myFilename;
+        var width = this.el.clientWidth,
+            height = Math.round(width / this.ratio),
+            padded = Math.round(document.body.clientHeight * 0.9);
 
-    fileTransfer.download(encodeURI(myUrl), filePath, (function (entry) {
-        var vid = document.getElementById("videoTurning");
-        vid.src = entry.nativeURL;
-        vid.loop = false;
-    }), (function (error) {
-        alert("Video download error: source " + error.source);
-        alert("Video download error: target " + error.target);
-    }), true, {
-        headers: {
-            Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+        // if the height is too big for the window, constrain it
+        if (height > padded) {
+            height = padded;
+            width = Math.round(height * this.ratio);
         }
-    });
-});
-*/
+
+        // set the width and height matching the aspect ratio
+        this.el.style.width = width + 'px';
+        this.el.style.height = height + 'px';
+
+        return {
+            width: width,
+            height: height
+        };
+    },
+    plugins: function (pageNum) {
+        'use strict';
+        // run the plugin
+        $(this.el).turn({
+            page: 1,
+            display: "single",
+            gradients: false,
+            acceleration: true
+        });
+        // hide the body overflow
+        document.body.className = 'hide-overflow';
+    }
+};
+
 
 //video file setup function
 var app = {
@@ -172,33 +127,82 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function () {
-        if ("Android" === device.platform) {
-            //video src set
-            var myFilename = "turning.mp4";
-            var myUrl = cordova.file.applicationDirectory + "www/video/" + myFilename;
-            var fileTransfer = new FileTransfer();
-            var filePath = cordova.file.dataDirectory + myFilename;
-            alert('myUrl : ' + myUrl);
-            alert('filePath : ' + filePath);
 
-            fileTransfer.download(encodeURI(myUrl), filePath,
-                function (entry) {
-                    var vid = document.getElementById("videoTurning");
-                    vid.src = entry.nativeURL;
-                    vid.loop = false;
-                    alert('download success');
-                    alert('entry,nativeURL : ' + entry.nativeURL);
-                    alert('vid src : ' + vid.src);
-                },
-                function (error) {
-                    alert("Video download error: source " + error.source);
-                    alert("Video download error: target " + error.target);
-                }, true, {
-                    headers: {
-                        Authorization: "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+        //okgo link function added
+        var linkfunc = function () {
+            $("#okgoLink").tap(function () {
+                var ref = window.open('http://okgo.net/wotw', '_blank', 'location=yes');
+            });
+        };
+
+        //Enable swiping...
+        $(".t").swipe({
+            //Generic swipe handler for all directions
+            swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+                if (direction === 'left') {
+                    $("#book").turn("next");
+                } else {
+                    $("#book").turn("previous");
+                }
+            },
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold: 75
+        });
+
+        if ("Android" === device.platform) {
+            document.addEventListener("deviceready", gotFile, false);
+
+            function gotFile() {
+                var fileTransfer = new FileTransfer();
+                var uri = encodeURI('cdvfile://localhost/temporary/turning.mp4');
+
+                fileTransfer.download(
+                    "file:///android_asset/www/video/turning.mp4",
+                    uri,
+                    function (entry) {
+                        resolveLocalFileSystemURL('cdvfile://localhost/temporary/turning.mp4', function (entry1) {
+                            var nativePath = entry1.toURL();
+                            document.querySelector("#videoTurning").src = nativePath;
+                            //file
+                            document.addEventListener("deviceready", gotFile_okgo, false);
+                        });
+                    },
+                    function (error) {
+                        alert('download error source ' + error.source);
                     }
-                });
-            alert('download ended');
+                );
+            }
+
+            function gotFile_okgo() {
+                var fileTransfer = new FileTransfer();
+                var uri = encodeURI('cdvfile://localhost/temporary/okgo.mp4');
+
+                fileTransfer.download(
+                    "file:///android_asset/www/video/okgo.mp4",
+                    uri,
+                    function (entry) {
+                        resolveLocalFileSystemURL('cdvfile://localhost/temporary/okgo.mp4', function (entry1) {
+                            var nativePath = entry1.toURL();
+                            document.querySelector("#videoOkgo").src = nativePath;
+                            //file
+                            bookMake();
+                        });
+                    },
+                    function (error) {
+                        alert('download error source ' + error.source);
+                    }
+                );
+            }
+
+            function bookMake() {
+                var loadingPop = document.getElementById("loadingDiv");
+                loadingPop.style.display = 'none';
+                module.init('book');
+            }
+
+            function fail(error) {
+                alert('Error creating file [' + error.name + ']: ' + error.message);
+            }
         }
     }
 };
